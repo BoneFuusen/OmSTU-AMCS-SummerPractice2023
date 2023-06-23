ece6d788d194a609dbab68a8721d5f76cee8f957
BoneFuusen <kotov_sergey04@mail.ru>
2023-06-23 23:44:41 +0600

    test5

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -46,11 +46,11 @@ public class SquareEquation
         if (d < 0)
         {
             if (d > -eps){
-                double[] answer = new double[2];
+                double[] answer = new double[1];
 
                 double x1 = -(b + Math.Sign(b) * Math.Sqrt(d))/(2*a);
 
-                answer[0] = x1; answer[1] = x1;
+                answer[0] = x1;
 
                 return answer;
             }
