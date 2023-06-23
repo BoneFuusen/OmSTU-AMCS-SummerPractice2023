cf7734d83192390e4ed2beea06375732ec748b6a
BoneFuusen <kotov_sergey04@mail.ru>
2023-06-23 15:48:23 +0600

    change_test

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,7 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
+        double[] result = new double[2];
         throw new NotImplementedException();
     }
 }
