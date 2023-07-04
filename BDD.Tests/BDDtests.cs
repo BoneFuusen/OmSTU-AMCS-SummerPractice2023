7545f8ec2c2643174d0467a1f8cf11f6d941198b
BoneFuusen <kotov_sergey04@mail.ru>
2023-07-04 21:58:49 +0600

    final

--- BDD.Tests/BDDtests.cs
+++ BDD.Tests/BDDtests.cs
@@ -1,127 +1,127 @@
-using SquareEquationLib;
-using TechTalk.SpecFlow;
+using TechTalk.SpecFlow; 
+using SquareEquationLib; 
+using Xunit; 
 
-namespace BDD.Tests{
+[Binding] 
+public class Steps { 
 
-    [Binding]
-    public class BDD_Tests
-    {
-        double a; double b; double c; double[] res;
-        [Given(@"Квадратное уравнение с коэффициентами \((.*)e(.*), (.*), (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиE(int p0, int p1, int p2, int p3)
-        {
-            a = 1e-7; c = p2; b = p3;
-        }
+    private double a, b, c; 
+    private double[] result = {}; 
 
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), NaN\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиNaN_j(int p0, int p1)
-        {
-            a = p0; b = p1; c = double.NaN;
-        }
-        
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), Double\.NegativeInfinity\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_NegativeInfinity_opa(int p0, int p1)
-        {
-            a = p0; b = p1; c = double.NegativeInfinity;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), Double\.PositiveInfinity\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_PositiveInfinity3(double p0, double p1) { 
+    this.a = p0; 
+    this.b = p1; 
+    this.c = Double.PositiveInfinity; 
+    } 
 
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентами_baba(int p0, int p1, int p2)
-        {
-            a = p0; b = p1; c = p2;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), Double\.NegativeInfinity\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_NegativeInfinity3(double p0, double p1) { 
+    this.a = p0; 
+    this.b = p1; 
+    this.c = Double.NegativeInfinity; 
+    } 
 
-        [Given(@"Квадратное уравнение с коэффициентами \(NaN, (.*), (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиNaNlo(int p0, int p1)
-        {
-            a = double.NaN; b = p0; c = p1;
-        }
-        
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), Double\.PositiveInfinity, (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_PositiveInfinity_b(int p0, int p1)
-        {
-            a = p0; c = p1; b = double.PositiveInfinity;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \(NaN, (.*), (.*)\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиNaN1(double p0, double p1) 
+    { 
+    this.a = double.NaN; 
+    this.b = p0; 
+    this.c = p1; 
+    } 
 
-        [Given(@"Квадратное уравнение с коэффициентами \(Double\.PositiveInfinity, (.*), (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_PositiveInfinity_a(int p0, int p1)
-        {
-            a = double.PositiveInfinity; b = p0; c = p1;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \(Double\.NegativeInfinity, (.*), (.*)\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_NegativeInfinity1(double p0, double p1) 
+    { 
+    this.a = double.NegativeInfinity; 
+    this.b = p0; 
+    this.c = p1; 
+    } 
 
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), NaN, (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиNaNi(int p0, int p1)
-        {
-            a = p0; b = double.NaN; c = p1;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), NaN\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиNaN3(double p0, double p1) 
+    { 
+    this.a = p0; 
+    this.b = p1; 
+    this.c = double.NaN; 
+    } 
 
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), Double\.NegativeInfinity, (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_NegativeInfinity_boba(int p0, int p1)
-        {
-            a = p0; b = double.NegativeInfinity; c = p1;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), Double\.PositiveInfinity, (.*)\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_PositiveInfinity2(double p0, double p1) 
+    { 
+    this.a = p0; 
+    this.b = double.PositiveInfinity; 
+    this.c = p1; 
+    } 
 
-        [Given(@"Квадратное уравнение с коэффициентами \(Double\.NegativeInfinity, (.*), (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_NegativeInfinity_biba(int p0, int p1)
-        {
-            a = double.NegativeInfinity; b = p0; c = p1;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), Double\.NegativeInfinity, (.*)\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_NegativeInfinity2(double p0, double p1) 
+    { 
+    this.a = p0; 
+    this.b = double.NegativeInfinity; 
+    this.c = p1; 
+    } 
 
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), Double\.PositiveInfinity\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_PositiveInfinity_gfg(int p0, int p1)
-        {
-            a = p0; b = p1; c = double.PositiveInfinity;
-        }
-        
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентами_aaa(int p0, int p1, int p2)
-        {
-            a = p0; b = p1; c = p2;
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентами(double p0, double p1, double p2) 
+    { 
+    this.a = p0; 
+    this.b = p1; 
+    this.c = p2; 
+    } 
 
-        
-        [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
-        public void ДопустимКвадратноеУравнениеСКоэффициентами_popa(int p0, int p1, int p2)
-        {
-            a = p0; b = p1; c = p2;
-        }
-        
-        [When(@"вычисляются корни квадратного уравнения")]
-        public void КогдаВычисляютсяКорниКвадратногоУравнения()
-        {
-            try{
-            res = SquareEquation.Solve(a, b, c);
-            } catch{}
-        }
-        
-        [Then(@"выбрасывается исключение ArgumentException")]
-        public void ТоВыбрасываетсяИсключениеArgumentException()
-        {
-            Assert.Throws<System.ArgumentException>(() => SquareEquation.Solve(a, b, c));
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), NaN, (.*)\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиNaN2(double p0, double p1) 
+    { 
+    this.a = p0; 
+    this.b = double.NaN; 
+    this.c = p1; 
+    } 
 
-        [Then(@"множество корней квадратного уравнения пустое")]
-        public void ТоМножествоКорнейКвадратногоУравненияПустое()
-        {
-            Assert.True(res == Array.Empty<double>());
-        }
+    [Given(@"Квадратное уравнение с коэффициентами \(Double\.PositiveInfinity, (.*), (.*)\)")] 
+    public void ДопустимКвадратноеУравнениеСКоэффициентамиDouble_PositiveInfinity1(double p0, double p1) 
+    { 
+    this.a = double.PositiveInfinity; 
+    this.b = p0; 
+    this.c = p1; 
+    } 
 
-        [Then(@"квадратное уравнение имеет один корень (.*) кратности два")]
-        public void ТоКвадратноеУравнениеИмеетОдинКореньКратностиДва(int p0)
-        {
-            double[] answer = {p0}; 
-            Array.Sort(answer); 
-            Array.Sort(res); 
-            Assert.Equal(answer, res);
-        }
 
-        [Then(@"квадратное уравнение имеет два корня \((.*), (.*)\) кратности один")]
-         public void ТоКвадратноеУравнениеИмеетДваКорняКратностиОдин(int p0, int p1)
-         {
-            double[] answer = {p0, p1}; 
-            Array.Sort(answer); 
-            Array.Sort(res); 
-            Assert.Equal(answer, res);
-         }
-    }
+    [When(@"вычисляются корни квадратного уравнения")] 
+    public void КогдаВычисляютсяКорниКвадратногоУравнения() 
+    { 
+    try { 
+    result = SquareEquation.Solve(this.a, this.b, this.c); 
+    } catch { 
+    } 
+    } 
+
+    [Then(@"квадратное уравнение имеет два корня \((.*), (.*)\) кратности один")] 
+    public void ТоКвадратноеУравнениеИмеетДваКорняКратностиОдин(double p0, double p1) 
+    { 
+    double[] answer = {p0, p1}; 
+    Array.Sort(answer); 
+    Array.Sort(result); 
+    Assert.Equal(answer, result); 
+    } 
+
+    [Then(@"квадратное уравнение имеет один корень (.*) кратности два")] 
+    public void ТоКвадратноеУравнениеИмеетОдинКореньКратностиДва(double p0) 
+    { 
+    double[] answer = {p0}; 
+    Assert.Equal(answer, result); 
+    } 
+
+    [Then(@"множество корней квадратного уравнения пустое")] 
+    public void ТоМножествоКорнейКвадратногоУравненияПустое() 
+    { 
+    double[] answer = {}; 
+    Assert.Equal(answer, result); 
+    } 
+
+    [Then(@"выбрасывается исключение ArgumentException")] 
+    public void ТоВыбрасываетсяИсключениеArgumentException() 
+    { 
+    Assert.Throws<System.ArgumentException>(() => SquareEquation.Solve(a, b, c)); 
+    } 
 }
